b6998c04c3232a549ba2b040416bf565555f1d79
L-cloud <whiteleo96@gmail.com>
2022-10-23 12:16:40 +0000

    set daphe, asgi server

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -1,28 +1,16 @@
-"""
-ASGI config for mysite project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
-"""
-
 import os
-
 from channels.auth import AuthMiddlewareStack
-from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-
-django_asgi_app = get_asgi_application()
-
+from django.core.asgi import get_asgi_application
 import chat.routing
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
 application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
-  "websocket": AllowedHostsOriginValidator(
-    AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
-  ),
-})
+              "http": get_asgi_application(),
+                "websocket": AuthMiddlewareStack(
+                            URLRouter(
+                                            chat.routing.websocket_urlpatterns
+                                                    )
+                                ),
+                })
